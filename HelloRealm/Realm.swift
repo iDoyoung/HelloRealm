c0523626dd0e7200186699ce945e70276a4afb7e
DoYoung Lee <ido.young@icloud.com>
2021-02-03 23:57:30 +0900

    Initial Commit

---
+++ HelloRealm/Realm.swift
@@ -0,0 +1,75 @@
+//
+//  Realm.swift
+//  HelloRealm
+//
+//  Created by ido on 2021/02/03.
+//
+
+import Foundation
+import RealmSwift
+
+//MARK: - Object 생성
+/*  dynamic : Runtime 일때, Realm이 자동으로 변수의 변화를 탐지하기 위해
+    @objc : 위 과정이 objective-c를 통해 이루어지기 때문에 @objc 키워드를 덧붙입니다.*/
+class MyData: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var jobPosition = ""
+    var careers = List<String>()
+}
+
+struct RealmModel {
+    
+    //MARK: - Default Realm
+    let realm = try! Realm()
+    
+    //MARK: - Create Data
+    func create(_ data: MyData) {
+        do {
+            try realm.write {
+                realm.add(data)
+            }
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+        
+    //MARK: - Update Data
+    func update(_ data: MyData, jobPostion: String, career: List<String>) {
+        do {
+            try realm.write {
+                data.jobPosition = jobPostion
+                data.careers = career
+            }
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+    
+    //MARK: - Delete Data
+    func delete(_ data: MyData) {
+        do {
+            try realm.write {
+                realm.delete(data)
+            }
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+    
+    //MARK: - Filter & Sorting
+    func filering() -> MyData? {
+        let result = realm.objects(MyData.self).filter("name == Doyoung")
+        return result.first
+    }
+    
+    
+    //MARK: - 파일 확인
+    /* Realm Studio 설치 후*/
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        
+        return documentsDirectory
+    }
+
+}
---
+++ HelloRealm/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  HelloRealm
+//
+//  Created by ido on 2021/02/03.
+//
+
+import UIKit
+import RealmSwift
+
+class ViewController: UIViewController {
+
+    let model = RealmModel()
+    var resultDatas: Results<MyData>?
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var carrers = List<String>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        }
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var positionTextField: UITextField!
+    @IBOutlet weak var careerTextField: UITextField!
+    
+    
+    
+    @IBAction func saveData(_ sender: UIButton) {
+        let newData = MyData()
+        newData.careers = self.carrers
+        newData.jobPosition = positionTextField.text!
+        newData.name = nameTextField.text!
+        model.create(newData)
+        tableView.reloadData()
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultDatas?.count ?? 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? Cell else {
+            return UITableViewCell()
+        }
+        cell.nameLabel.text = resultDatas?[indexPath.row].name
+        cell.positionLabel.text = resultDatas?[indexPath.row].jobPosition
+        cell.careerCount.text = String(resultDatas?[indexPath.row].careers.count ?? 0)
+        
+        return cell
+    }
+    
+    
+}
+
+class Cell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var positionLabel: UILabel!
+    @IBOutlet weak var careerCount: UILabel!
+}
